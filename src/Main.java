fac9c91f282c97473e903bf2d5e93f1287230e8f
Iury-Ghub <joseiury74@gmail.com>
2025-10-19 18:19:46 -0300

    Limpei a classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        Livro[] livros = new Livro[50];
-        Usuario[] usuarios = new Usuario[50];
-        Emprestimo[] emprestimos = new Emprestimo[100];
-
-        usuarios[0] = new Usuario("Iury","iury@gmail.com",581434);
-        livros[0] = new Livro("o mais belo carrapato", "Sarapatel", 2013, true);
-        emprestimos[0] = new Emprestimo(livros[0], usuarios[0]);
-
-        emprestimos[0].registrarDevolucao();
-        emprestimos[0].registrarDevolucao();
 
     }
 }
